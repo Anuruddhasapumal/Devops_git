1ee72d18142dd214566e55002cfe399101dd5185
ANURUDDHASE <sapumalanuruddha@gmail.com>
2023-10-19 14:33:06 +0530

    0

--- src/main/java/com/example/devops/DevopsRestCon.java
+++ src/main/java/com/example/devops/DevopsRestCon.java
@@ -1,12 +1,14 @@
 package com.example.devops;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class DevopsRestCon {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String hello() {
         return "welcome";
     }
